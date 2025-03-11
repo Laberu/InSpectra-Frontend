b18808f3c894f8fe9237362b3aaa6d154039308a
Tharunthorn Musik <tharunthornmusik1446@gmail.com>
2025-03-11 20:48:18 +0700

    Update next.config.mjs

--- inspection-app/next.config.mjs
+++ inspection-app/next.config.mjs
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+export default {
+    output: "standalone",
+    serverRuntimeConfig: {
+      port: 3002, // Set Next.js port
+    },
+  };
